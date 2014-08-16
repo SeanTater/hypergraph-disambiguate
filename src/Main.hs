710295bd85f478b041dc16351781337609cb2079
Sean Gallagher <stgallag@gmail.com>
2014-08-16 08:59:54 -0400

    another 5 fold speedup

--- src/Main.hs
+++ src/Main.hs
@@ -20,13 +20,13 @@ data WordCount = WordCount String Int deriving (Show)
 main = do
     text_database:_ <- getArgs 
     conn <- connectSqlite3 text_database
-    wordcounts <- quickQuery conn "SELECT word, para_id FROM word_counts LIMIT 1000" []
+    wordcounts <- quickQuery conn "SELECT word, para_id FROM word_counts LIMIT 10000" []
     
     let wc_as_wc = [ WordCount (fromSql w) (fromSql p) | (w : p : _) <- wordcounts ]
     let paragraphs = groupBy (\(WordCount w1 p1) (WordCount w2 p2)  -> p1 == p2) wc_as_wc
     let paragraph_tokens = [ [ w | WordCount w _  <- paragraph] | paragraph <- paragraphs]
     
-    let (RIndex.BloomMap _ bmap) = RIndex.indexBinaryChunks paragraph_tokens
+    (RIndex.BloomMap _ bmap) <- RIndex.indexBinaryChunks paragraph_tokens
     print $ M.keysSet bmap
     disconnect conn
 
